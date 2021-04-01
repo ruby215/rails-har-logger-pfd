5bed9c26bc8b8018e1b17d21a7be9b609f8dc647
ruby215 <Michaeldasher185@outlook.com>
2021-04-01 15:58:32 -0700

    Improvements to request side of HAR output
    Assume HTTP/1.1 when HTTP_VERSION is absent in environment.
    
    Calculate size of headers.
    
    Assume body length is 0 when CONTENT_LENGTH is absent in environment.

--- http_request.rb
+++ http_request.rb
@@ -8,26 +8,38 @@ module Akita
       req = Rack::Request.new env
 
       @self = {
-        method: req.request_method,
+        method: getMethod(env),
         url: req.url,
         httpVersion: getHttpVersion(env),
         cookies: getCookies(env),
         headers: getHeaders(env),
         queryString: getQueryString(env),
-        postData: getPostData(env),
-        headersSize: -1,  # Doesn't appear to be available in env.
+        headersSize: getHeadersSize(env),
         bodySize: getBodySize(env),
       }
+
+      # Augment with post data if we have any.
+      postData = getPostData(env)
+      if postData != nil then
+        @self[:postData] = postData
+      end
     end
 
     def to_json(*args)
       @self.to_json(*args)
     end
 
+    # Obtains the client's request method from an HTTP environment.
+    def getMethod(env)
+      (Rack::Request.new env).request_method
+    end
+
     # Obtains the client-requested HTTP version from an HTTP environment.
     def getHttpVersion(env)
-      # XXX This isn't populated when running with `rspec`.
-      env['HTTP_VERSION']
+      # The environment doesn't have HTTP_VERSION when running with `rspec`;
+      # assume HTTP/1.1 when this happens. We don't return nil, so we can
+      # calculate the size of the headers.
+      env.key?('HTTP_VERSION') ? env['HTTP_VERSION'] : 'HTTP/1.1'
     end
 
     # Builds a list of cookie objects from an HTTP environment.
@@ -70,9 +82,9 @@ module Akita
           #
           # XXX Spec has space for files, but are file uploads ever
           # URL-encoded?
-          result['params'] = HarUtils.hashToList req.params
+          result[:params] = HarUtils.hashToList req.params
         else
-          result['text'] = req.body.string
+          result[:text] = req.body.string
         end
 
         result
@@ -81,10 +93,31 @@ module Akita
       end
     end
 
-    # Obtains the size of the request body from an HTTP environment. Returns -1
-    # if this cannot be determined.
+    def getHeadersSize(env)
+      # XXX This seems to under-count, compared to a HAR produced by Firefox.
+
+      # Count the number of bytes needed to produce the first line of the
+      # request (HTTP method, full path, HTTP version, CRLF). For example,
+      #
+      #   GET /index.html?foo=bar&baz=qux HTTP/1.1<CR><LF>
+      req = Rack::Request::new env
+      line_length =
+        getMethod(env).length + 1
+          + req.fullpath.length + 1
+          + getHttpVersion(env).length + 2
+
+      # Add the size of the headers. Add 2 to the starting value to account for
+      # the CRLF on the blank line.
+      getHeaders(env).reduce(line_length + 2) { |accum, entry|
+        # Header-Name: header value<CR><LF>
+        accum + entry[:name].length + 2 + entry[:value].length + 2
+      }
+    end
+
+    # Obtains the size of the request body from an HTTP environment.
     def getBodySize(env)
-      env.key?('CONTENT_LENGTH') ? env['CONTENT_LENGTH'].to_i : -1
+      # Assume no content if Content-Length header was not provided.
+      env.key?('CONTENT_LENGTH') ? env['CONTENT_LENGTH'].to_i : 0
     end
   end
 end
